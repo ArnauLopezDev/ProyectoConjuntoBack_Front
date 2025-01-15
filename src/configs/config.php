a6ae9af8ff622e8bec1de1e2a7f1841d7d00c6d0
Arnau-Lopez <154209033+ArnauLopezDev@users.noreply.github.com>
2025-01-15 09:27:13 +0100

    Base de config file

---
+++ src/configs/config.php
@@ -0,0 +1,14 @@
+<?php
+//credenciales de la base de datos  -> a cambiar
+$bd_config = array(
+    'host' => 'db',
+//$host = '160.153.133.189';,
+    'db' => 'daw2',
+    'user' => 'root',
+    'pass' => 'Gimbernat1'
+);
+
+$blog_config = array(
+    'posts_por_pagina' => '3',
+    'img' => 'img/'
+);
